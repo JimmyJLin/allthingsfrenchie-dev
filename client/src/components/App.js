cec2dc399eaecfb302a8ed4c95ea1a35f6ded0f6
Jimmy Lin <usmcjimmy@gmail.com>
2017-09-29 09:37:39 -0400

    refactored landing page into seperate component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
-import Landing from './Landing';
+import Landing from './landing/Landing';
 
 class App extends Component {
   render() {
     return (
       <div>
         <BrowserRouter>
-          <div>
-            <Route exact path="/" component={Landing}/>
-          </div>
+          <Route exact path="/" component={Landing}/>
         </BrowserRouter>
       </div>
     );
